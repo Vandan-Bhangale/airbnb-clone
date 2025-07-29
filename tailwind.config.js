dfb52dc6b8a423720a8f7fa84c3af863f2a61d5c
Vandan Bhangale <159510950+Vandan-Bhangale@users.noreply.github.com>
2025-07-29 10:38:31 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [ './views/**/*.ejs', // Include all EJS files
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
